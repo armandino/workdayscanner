321626a453cf4df7a65dd15d374f0c596a6f017d
Arman Sharif <arman@thinkthru.ca>
2024-11-30 23:38:18 -0800

    Cleanup

---
+++ src/main/java/org/example/WorkdayTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.example.generated.WorkerDataType;
+
+public class WorkdayTest {
+
+    public static void main(String[] args) {
+        WorkerDataType workerDataType = new WorkerDataType();
+
+        System.out.println(workerDataType);
+    }
+
+}
--- targetSources/com/workday/sources/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:com.workday/bsvc", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.workday.sources;
